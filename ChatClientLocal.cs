b50482b4357370a12a394841b10a1368e492d8ba
yakkudev <65334490+yakkudev@users.noreply.github.com>
2025-08-06 19:52:05 +0200

    basic messaging

--- ChatClientLocal.cs
+++ ChatClientLocal.cs
@@ -2,40 +2,72 @@ using System.Net.Sockets;
 
 namespace ChatRoom;
 
-public class ChatClient {
+public class ChatClientLocal {
 
-    private string host;
-    private int port;
+    string host;
+    int port;
 
-    private TcpClient client;
+    TcpClient client;
 
-    public ChatClient(string host, int port) {
+    public ChatClientLocal(string host, int port) {
         this.host = host;
         this.port = port;
 
         Connect();
     }
 
-    private void Connect() {
-        Console.WriteLine($"connecting to {host}, port {port}");
-        var retries = 10;
-        while (client == null) {
-            try {
-                client = new TcpClient(host, port);
-            }
-            catch (Exception e){
-                if (e is SocketException && retries-- > 0) {
-                    Console.WriteLine("retrying connection...");
-                    Thread.Sleep(1000);
-                    continue;
-                }
-
-                Console.WriteLine("an error occured.");
-                return;
-            }
+    bool TryConnect() {
+        try {
+            client = new TcpClient(host, port);
+        }
+        catch (Exception){
+            return false;
         }
 
-        Console.WriteLine("###### CONNECTED ######"); 
-        var message = "PING!";
+        return true;
+    }
+
+    void Connect() {
+        Console.WriteLine($"connecting to {host}, port {port}");
+        var retries = 10;
+        while (!TryConnect()) {
+            if (--retries < 0) {
+                Console.WriteLine($"error: could not establish connection");
+                return;
+            }
+            Console.WriteLine($"retrying connection...");
+            Thread.Sleep(1000);
+        }
+
+        Console.WriteLine("###### CONNECTED ######");
+
+        var name = "anonymous";
+        using var stream = client.GetStream();
+        using var reader = new StreamReader(stream);
+        using var writer = new StreamWriter(stream);
+        writer.AutoFlush = true;
+        
+        var receiveThread = new Thread(() => {
+            string? line;
+            while ((line = reader.ReadLine()) != null) {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.WriteLine(line);
+                Console.Write("> ");
+                Console.Out.Flush();
+            }
+        });
+        receiveThread.Start();
+
+        string? input;
+        while ((input = Console.ReadLine()) != null) {
+            int currentPos = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.WriteLine($"#> {input}");
+            Console.SetCursorPosition(0, currentPos);
+            Console.Out.Flush();
+            
+            writer.WriteLine($"<{name}> {input}");
+            Console.Write("> ");
+        } 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ChatRoom;
+
+public static class Program {
+    public static void Main(string[] args) {
+        
+        if (args.Length == 0) {
+            _ = new ChatClientLocal("localhost", 21337);
+            return;
+        }
+
+        if (args.ElementAtOrDefault(0) == "--client") {
+            var portArg = args.ElementAtOrDefault(1) ?? "21337";
+            if (!ushort.TryParse(portArg, out var port)) {
+                Console.WriteLine("error: invalid port.");
+                return;
+            }
+            
+            var hostArg = args.ElementAtOrDefault(2);
+            if (hostArg == null) {
+                Console.WriteLine("error: host not specified");
+                return;
+            }
+            _ = new ChatClientLocal(hostArg, port);
+            return;
+        }
+
+        if (args.ElementAtOrDefault(0) == "--server") {
+            var portArg = args.ElementAtOrDefault(1);
+            if (portArg == null) {
+                Console.WriteLine("error: no port provided.");
+                return;
+            }
+            if (!ushort.TryParse(portArg, out var port)) {
+                Console.WriteLine("error: invalid port.");
+                return;
+            }
+
+            _ = new ChatServer(port);
+            return;
+        }
+        
+        Console.WriteLine("ChatRoom - client/server chat room application");
+        Console.WriteLine("usage:");
+        Console.WriteLine("<no args> - start client");
+        Console.WriteLine("--client <port> <host> - start client with arguments");
+        Console.WriteLine("--server <port> - start server at port");
+    }
+}
